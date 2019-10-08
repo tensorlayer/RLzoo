eb3361f3cd9cdd01f51467efbea78a8520a32237
Tokarev-TT-33 <34995488+Tokarev-TT-33@users.noreply.github.com>
2019-10-08 22:42:46 +0800

    Update __init__.py
    update ddpg, ppo_penalty, ppo_clip, dppo_penalty, dppo_clip

--- baselines/algorithms/__init__.py
+++ baselines/algorithms/__init__.py
@@ -2,9 +2,11 @@ from algorithms.ac.ac import AC
 from algorithms.pg.pg import PG
 from algorithms.dqn.dqn import DQN
 from algorithms.a3c.a3c import A3C
-# from algorithms.ddpg.ddpg import DDPG
+from algorithms.ddpg.ddpg import DDPG
 from algorithms.td3.td3 import TD3
 from algorithms.sac.sac import SAC
-# from algorithms.ppo.ppo import PPO
-# from algorithms.dppo.dppo import DPPO
+from algorithms.ppo_penalty.ppo_penalty import PPO_PENALTY
+from algorithms.ppo_clip.ppo_clip import PPO_CLIP
+from algorithms.dppo_penalty.dppo_penalty import DPPO_PENALTY
+from algorithms.dppo_clip.dppo_clip import DPPO_CLIP
 # from algorithms.trpo.trpo import TRPO
