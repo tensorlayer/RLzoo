dc6e7ea1027a55de7e231bed184e330b5f5098d9
Tianyang Yu <34995488+Tokarev-TT-33@users.noreply.github.com>
2019-10-28 23:01:07 +0800

    update distributions.py
    Update set_param as output layer of StochasticPolicyNetwork policy_networks.py has changed.
    Add expend_dim in neglogp kl and entropy functions

--- baselines/common/distributions.py
+++ baselines/common/distributions.py
@@ -3,10 +3,22 @@ import numpy as np
 import tensorflow as tf
 from gym import spaces
 import copy
+from functools import wraps
+
+
+def expand_dims(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        result = tf.expand_dims(result, axis=-1)
+        return result
+
+    return wrapper
 
 
 class Distribution(object):
     """A particular probability distribution"""
+
     def set_param(self, *args, **kwargs):
         raise NotImplementedError
 
@@ -33,6 +45,7 @@ class Distribution(object):
 
 class Categorical(Distribution):
     """Creates a categorical distribution"""
+
     def __init__(self, ndim, logits=None):
         """
         Args:
@@ -41,7 +54,10 @@ class Categorical(Distribution):
         """
         self._ndim = ndim
         self._logits = logits
-        self.output_dim = ndim
+
+    @property
+    def ndim(self):
+        return copy.copy(self._ndim)
 
     def set_param(self, logits):
         """
@@ -67,6 +83,7 @@ class Categorical(Distribution):
     def logp(self, x):
         return -self.neglogp(x)
 
+    @expand_dims
     def neglogp(self, x):
         x = np.array(x)
         if np.any(x % 1):
@@ -75,6 +92,7 @@ class Categorical(Distribution):
         x = tf.one_hot(x, self._ndim, axis=-1)
         return tf.nn.softmax_cross_entropy_with_logits(x, self._logits)
 
+    @expand_dims
     def kl(self, logits):
         """
         Args:
@@ -90,6 +108,7 @@ class Categorical(Distribution):
         return tf.reduce_sum(
             p0 * (a0 - tf.math.log(z0) - a1 + tf.math.log(z1)), axis=-1)
 
+    @expand_dims
     def entropy(self):
         a0 = self._logits - tf.reduce_max(self._logits, axis=-1, keepdims=True)
         ea0 = tf.exp(a0)
@@ -100,6 +119,7 @@ class Categorical(Distribution):
 
 class DiagGaussian(Distribution):
     """Creates a diagonal Gaussian distribution """
+
     def __init__(self, ndim, mean_logstd=None):
         """
         Args:
@@ -110,19 +130,20 @@ class DiagGaussian(Distribution):
         self.mean = None
         self.logstd = None
         self.std = None
-        self.output_dim = 2*ndim
         if mean_logstd is not None:
             self.set_param(mean_logstd)
 
-    def set_param(self, param):
+    @property
+    def ndim(self):
+        return copy.copy(self._ndim)
+
+    def set_param(self, mean_logstd):
         """
         Args:
-            param (tensor): mean or mean and logstd parameters, stacked on the last axis
+            mean_logstd (tensor): mean and log std
         """
-        mean, logstd = tf.split(param, 2, -1)
-        self.mean = mean
-        self.logstd = logstd
-        self.std = tf.math.exp(logstd)
+        self.mean, self.logstd = mean_logstd
+        self.std = tf.math.exp(self.logstd)
 
     def get_param(self):
         """ Get parameters """
@@ -139,24 +160,24 @@ class DiagGaussian(Distribution):
     def logp(self, x):
         return -self.neglogp(x)
 
+    @expand_dims
     def neglogp(self, x):
-        return 0.5 * tf.reduce_sum(
-            tf.square((x - self.mean) / self.std), axis=-1) \
-            + 0.5 * np.log(2.0 * np.pi) * float(self._ndim) \
-            + tf.reduce_sum(self.logstd, axis=-1)
+        return 0.5 * tf.reduce_sum(tf.square((x - self.mean) / self.std), axis=-1) \
+                 + 0.5 * np.log(2.0 * np.pi) * float(self._ndim) + tf.reduce_sum(self.logstd, axis=-1)
 
+    @expand_dims
     def kl(self, mean_logstd):
         """
         Args:
-            mean_logstd (tensor): mean and logstd of another distribution,
-                                  stacked on the last axis
+            mean_logstd (tensor): mean and logstd of another distribution
         """
-        mean, logstd = tf.split(mean_logstd, 2, -1)
+        mean, logstd = mean_logstd
         return tf.reduce_sum(
             logstd - self.logstd +
             (tf.square(self.std) + tf.square(self.mean - mean))
             / (2.0 * tf.square(tf.math.exp(logstd))) - 0.5, axis=-1)
 
+    @expand_dims
     def entropy(self):
         return tf.reduce_sum(
             self.logstd + 0.5 * np.log(2.0 * np.pi * np.e), axis=-1)
@@ -174,4 +195,3 @@ def make_dist(ac_space):
         return DiagGaussian(ac_space.shape[0])
     else:
         raise NotImplementedError
-
