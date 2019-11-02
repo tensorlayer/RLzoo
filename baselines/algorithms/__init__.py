9bf385faa372561cecc89e0343cc22644150038e
Tianyang Yu <34995488+Tokarev-TT-33@users.noreply.github.com>
2019-11-02 18:39:22 +0800

    Update __init__.py
    Update PPO, DPPO

--- baselines/algorithms/__init__.py
+++ baselines/algorithms/__init__.py
@@ -5,8 +5,10 @@ from algorithms.a3c.a3c import A3C
 from algorithms.ddpg.ddpg import DDPG
 from algorithms.td3.td3 import TD3
 from algorithms.sac.sac import SAC
+from algorithms.ppo.ppo import PPO
 from algorithms.ppo_penalty.ppo_penalty import PPO_PENALTY
 from algorithms.ppo_clip.ppo_clip import PPO_CLIP
+from algorithms.dppo.dppo import DPPO
 from algorithms.dppo_penalty.dppo_penalty import DPPO_PENALTY
 from algorithms.dppo_clip.dppo_clip import DPPO_CLIP
 # from algorithms.trpo.trpo import TRPO
