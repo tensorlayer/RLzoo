e46a55a8cd6f05e5f40e1c1e3f6ecfb8dedf807f
Tianyang Yu <34995488+Tokarev-TT-33@users.noreply.github.com>
2019-11-06 00:59:20 +0800

    Update default.py

--- baselines/algorithms/ppo/default.py
+++ baselines/algorithms/ppo/default.py
@@ -75,7 +75,7 @@ def atari(env, default_seed=True):
 def classic_control(env, default_seed=True):
     if default_seed:
         # reproducible
-        seed = 5
+        seed = 1
         set_seed(seed, env)
 
     alg_params = dict(epsilon=0.2,     # for method 'clip'
@@ -83,7 +83,7 @@ def classic_control(env, default_seed=True):
                       lam=0.5)         # for method 'penalty'
 
     if alg_params.get('net_list') is None:
-        num_hidden_layer = 2  # number of hidden layers for the networks
+        num_hidden_layer = 1  # number of hidden layers for the networks
         hidden_dim = 100  # dimension of hidden layers for the networks
         with tf.name_scope('PPO'):
             with tf.name_scope('V_Net'):
@@ -96,12 +96,12 @@ def classic_control(env, default_seed=True):
         alg_params['net_list'] = net_list
 
     if alg_params.get('optimizers_list') is None:
-        actor_lr = 1e-3
-        critic_lr = 2e-3
+        actor_lr = 1e-4
+        critic_lr = 2e-4
         optimizers_list = [tf.optimizers.Adam(critic_lr), tf.optimizers.Adam(actor_lr)]
         alg_params['optimizers_list'] = optimizers_list
 
-    learn_params = dict(train_episodes=200,
+    learn_params = dict(train_episodes=1000,
                         test_episodes=100,
                         max_steps=200,
                         save_interval=10,
