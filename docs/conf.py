12487a3c04b7449b679325fd54684c8ba89ae72b
quantumiracle <1402434478@qq.com>
2020-05-05 23:01:07 -0400

    add logo

--- docs/conf.py
+++ docs/conf.py
@@ -91,6 +91,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_logo = './img/rlzoo-logo.png'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
