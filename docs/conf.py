09d27bb3d52adba5f69e1b36298459cecaf992a6
quantumiracle <1402434478@qq.com>
2020-05-21 00:40:06 -0400

    update doc

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = '2020, Zihan Ding, Tianyang Yu, Yanhua Huang, Hongming Zhang, Hao Do
 author = 'Zihan Ding, Tianyang Yu, Yanhua Huang, Hongming Zhang, Hao Dong'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0.2'
 
 
 # -- General configuration ---------------------------------------------------
