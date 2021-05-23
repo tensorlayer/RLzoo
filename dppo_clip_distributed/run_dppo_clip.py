d08778badf5584cc5fba4ebe0e9f84a0445c4104
quantumiracle <1402434478@qq.com>
2021-05-23 23:27:38 +0800

    update run_dppo_clip

--- dppo_clip_distributed/run_dppo_clip.py
+++ dppo_clip_distributed/run_dppo_clip.py
@@ -51,10 +51,11 @@ def make_infer_server_process(
 
 
 def make_learner_process(
-        num, build_net_func, traj_queue, grad_queue, should_stop_event, should_update_event, barrier, net_param_pipe):
+        num, build_net_func, traj_queue, grad_queue, should_stop_event, should_update_event, barrier,
+        net_param_pipe_list):
     process_list = []
-    for _ in range(num):
-        learner = DPPOLearner(build_net_func, net_param_pipe)
+    for i in range(num):
+        learner = DPPOLearner(build_net_func, net_param_pipe_list[i])
         p = mp.Process(
             target=learner.run, args=(traj_queue, grad_queue, should_stop_event, should_update_event, barrier))
         p.daemon = True
@@ -71,3 +72,46 @@ def make_global_manager(
     return [p], param_pipe_b
 
 
+if __name__ == '__main__':
+
+    n_sampler = 3
+    name = 'DPPO_CLIP'
+    build_env = partial(build_env, ('CartPole-v0', 'classic_control'))
+    env = build_env()
+    observation_space, action_space = env.observation_space, env.action_space
+    build_network = partial(make_network, (observation_space, action_space, name))
+
+    traj_queue = mp.Queue(maxsize=10000)
+    should_stop_event = mp.Event()
+    should_stop_event.clear()
+    should_update_event = mp.Event()
+    should_update_event.clear()
+    grad_queue = mp.Queue(maxsize=10000), mp.Queue(maxsize=10000),
+
+    n_learner = 2
+    barrier = mp.Barrier(2 + n_learner)  # InferServer + Updater + GlobalManager
+
+    process_list = []
+
+    p_list, param_pipe_b = make_global_manager(
+        build_env, make_opt, n_learner + 1, traj_queue, grad_queue, should_stop_event, should_update_event, barrier)
+    process_list.extend(p_list)
+
+    p_list, sample_pipe_list = make_sampler_process(
+        n_sampler, build_env, should_stop_event
+    )
+    process_list.extend(p_list)
+
+    p_list = make_infer_server_process(
+        build_network, build_env, sample_pipe_list, traj_queue, should_stop_event, should_update_event, barrier,
+        param_pipe_b[0])
+    process_list.extend(p_list)
+
+    p_list = make_learner_process(
+        n_learner, build_network, traj_queue, grad_queue, should_stop_event, should_update_event, barrier,
+        param_pipe_b[1:]
+    )
+    process_list.extend(p_list)
+
+    while True:
+        print(grad_queue[0].qsize)
